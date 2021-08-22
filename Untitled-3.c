7fc2c39d8931056b968b8ce3f935dd08dd19f389
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2021-08-22 15:03:45 +0530

    Add files via upload

---
+++ Untitled-3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    printf("choose a no:1\n2\n3\n4\n5);
+    int choice=0;
+    scanf("%d",&choice);
+}
+switch(choice)
+{
+    case 1:
+    printf("Pizza\nRs239");
+    break;
+    case 2:
+    printf("burger\nRs129");
+    break;
+
+    case 3:
+    printf("Pasta\nRs170");
+    break;
+
+    case 4:
+    printf("french fries\nRs99");
+    break;
+    case 5:
+    printf("sandwitch\nRs149");
+    break;
+
+}
